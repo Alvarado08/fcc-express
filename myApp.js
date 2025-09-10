6ea53961efb16fbe394855846043d50144ddaa6b
Alvarado08 <jorgealvarado0238@gmail.com>
2025-09-10 18:07:03 -0500

    Root level request logger middleware

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 let express = require("express");
 let app = express();
 
@@ -6,6 +7,12 @@ console.log("Hello, World");
 // app.get("/", (req, res) => {
 //   res.send("Hello, Express");
 // });
+function logger(req, res, next) {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+}
+
+app.use(logger);
 // Normal usage
 //app.use(express.static(__dirname + "/public"));
 
@@ -14,6 +21,13 @@ app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/json", (req, res) => {
+  res.json({
+    message:
+      process.env.MESSAGE_STYLE === "uppercase" ? "HELLO JSON" : "Hello json",
+  });
 });
 
 module.exports = app;
