c8720c11c73baf668bd30f8376a8e18d314beaed
Alvarado08 <jorgealvarado0238@gmail.com>
2025-09-09 23:19:15 -0500

    Starting a server and serving content

--- myApp.js
+++ myApp.js
@@ -3,4 +3,17 @@ let app = express();
 
 console.log("Hello, World");
 
+// app.get("/", (req, res) => {
+//   res.send("Hello, Express");
+// });
+// Normal usage
+//app.use(express.static(__dirname + "/public"));
+
+// Assets at the /public route
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
 module.exports = app;
